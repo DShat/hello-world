ecdfddb6cbe321325a0c59180ea81902ad037e8c
DShat <36413993+DShat@users.noreply.github.com>
2018-02-16 10:02:36 -0500

    Put spaces before curly braces

--- 07/YourInitials.java
+++ 07/YourInitials.java
@@ -1,5 +1,5 @@
-public class YourInitials{
-	public static void main(String[] args){
+public class YourInitials {
+	public static void main(String[] args) {
 		System.out.println("For the name Derek Shat...");
 		System.out.println("DDDD   SSS ");
 		System.out.println("D   D S   S");
