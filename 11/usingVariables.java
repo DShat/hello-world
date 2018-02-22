a2f749ee91b2fc71e4a417c66014926c1bddbf92
DShat <36413993+DShat@users.noreply.github.com>
2018-02-22 11:01:39 -0500

    Add files via upload

---
+++ 11/usingVariables.java
@@ -0,0 +1,10 @@
+public class usingVariables{
+    public static void main(String [] args){
+        int roomNumber = 113;
+        double valueCloseTo = 2.71828;
+        String learningAbout = "Computer Science";
+        System.out.println("This is room # " + roomNumber);
+        System.out.println("e is close to " + valueCloseTo);
+        System.out.println("I am learning a bit about " + learningAbout);
+    }
+ }
