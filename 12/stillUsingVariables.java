b22b9cfff9b46224903d244b49026fafac806ef0
DShat <36413993+DShat@users.noreply.github.com>
2018-02-23 10:05:02 -0500

    Add files via upload

---
+++ 12/stillUsingVariables.java
@@ -0,0 +1,7 @@
+public class stillUsingVariables{
+    public static void main(String [] args){
+        String myName = "Derek";
+        int graduatingYear = 2019;
+        System.out.println("My name is " + myName + " and I'll graduate in " + graduatingYear + ".");
+    }
+}
