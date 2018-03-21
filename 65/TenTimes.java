d4694d30f0855c74cc9d3f60865a59d3f03e3e53
DShat <36413993+DShat@users.noreply.github.com>
2018-03-21 11:05:46 -0400

    Add files via upload

---
+++ 65/TenTimes.java
@@ -0,0 +1,7 @@
+public class TenTimes{
+    public static void main(String[] args){
+        for (int x = 1 ; x <= 10 ; x += 1) {
+            System.out.println(x + ". Mr Gallo is cool.");
+            }
+        }
+    }
---
+++ 66/CountingMachine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CountingMachine{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Count to: ");
+        int x = sc.nextInt();
+        for(int y = 0; y <= x; y += 1){
+            System.out.print(y + " ");
+            }
+        }
+    } 
+        
+        
